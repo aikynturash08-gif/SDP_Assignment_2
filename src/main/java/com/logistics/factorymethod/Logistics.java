72ac3717ffe9bd1267ef638f9fb1d8fe83c97933
aikynturash08-gif <aikynturash08@gmail.com>
2026-09-24 23:20:23 +0500

    Update Logistics.java

--- src/main/java/com/logistics/factorymethod/Logistics.java
+++ src/main/java/com/logistics/factorymethod/Logistics.java
@@ -1,4 +1,10 @@
 package main.java.com.logistics.factorymethod;
 
-public class Logistics {
+public abstract class Logistics {
+    public abstract Transport createTransport();
+
+    public void planDelivery(String cargo, String destination) {
+        Transport transport = createTransport();
+        transport.deliver(cargo, destination);
+    }
 }
