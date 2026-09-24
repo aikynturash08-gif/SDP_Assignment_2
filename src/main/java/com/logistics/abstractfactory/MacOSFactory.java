f752ffc706b21c53ab5867475d6268ee2a3955d7
aikynturash08-gif <aikynturash08@gmail.com>
2026-09-24 23:16:42 +0500

    Update MacOSFactory.java

--- src/main/java/com/logistics/abstractfactory/MacOSFactory.java
+++ src/main/java/com/logistics/abstractfactory/MacOSFactory.java
@@ -1,4 +1,13 @@
 package main.java.com.logistics.abstractfactory;
 
-public class MacOSFactory {
-}
+public class MacOSFactory implements GUIFactory {
+    @Override
+    public Button createButton() {
+        return new MacOSButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new MacOSCheckbox();
+    }
+}s
